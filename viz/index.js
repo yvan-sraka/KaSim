301b901f83fbbf063741422bece1f2d975bd4c09
Yvan Sraka <yvan@sraka.pw>
2019-04-15 23:05:24 +0200

    WIP

---
+++ viz/index.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+import 'jquery-ui';
+import 'bootstrap';
+
+import CodeMirror from 'codemirror';
+window.CodeMirror = CodeMirror;
+
+import 'd3';
+import 'dagre-d3';
---
+++ viz/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, '.')
+  },
+  // externals: {
+  //   jquery: 'jQuery',
+  //   codemirror: 'CodeMirror'
+  // }
+};
